9c6bdf7950391dd3a053707dc9717f46a9d2fe4f
bruscalia <64752094+bruscalia@users.noreply.github.com>
2025-01-08 00:07:31 -0300

    CHORE: PFSSP Branch and Bound with Age and remove specific algorithms cython

--- src/bnbprob/pfssp/bnb.py
+++ src/bnbprob/pfssp/bnb.py
@@ -1,6 +1,4 @@
-from bnbprob.pfssp.cython.node import Node
-
-# from bnbprob.pfssp.cython.search import BranchAndBound
+from bnbpy.cython.node import Node
 from bnbpy.search import BranchAndBound
 
 RESTART = 10_000
@@ -45,6 +43,44 @@ class CallbackBnB(LazyBnB):
 
     def dequeue(self) -> Node:
         if self.explored % self.restart_freq == 0:
+            pri, node = min(self.queue, key=lambda x: x[-1].lb)
+            self.queue.remove((pri, node))
+            return node
+        return super().dequeue()
+
+
+class CallbackBnBAge(LazyBnB):
+
+    restart_freq: int
+    """Number of nodes dequeued since last improvement"""
+    sol_age: int
+    """Frequency in which restarts from best bound are called"""
+
+    def __init__(
+        self,
+        rtol=0.0001,
+        atol=0.0001,
+        eval_node='in',
+        save_tree=False,
+        restart_freq=RESTART,
+    ):
+        super().__init__(rtol, atol, eval_node, save_tree)
+        self.restart_freq = restart_freq
+        self.sol_age = 0
+
+    def solution_callback(self, node: Node):
+        self.sol_age = 0
+        new_sol = node.problem.local_search()
+        if new_sol is not None:
+            # General procedure in case is valid
+            if new_sol.is_feasible() and new_sol.lb < node.solution.lb:
+                node.set_solution(new_sol)
+                node.check_feasible()
+                self.set_solution(node)
+
+    def dequeue(self) -> Node:
+        self.sol_age += 1
+        if (self.sol_age % self.restart_freq) == 0:
             pri, node = min(self.queue, key=lambda x: x[-1].lb)
             self.queue.remove((pri, node))
             return node
